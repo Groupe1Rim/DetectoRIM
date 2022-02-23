e40bf999d055dab7dff1636ce6df0eba03f1084c
Groupe1Rim <94782897+Groupe1Rim@users.noreply.github.com>
2022-02-23 11:59:26 +0100

    Update SNP_Calculator.py

--- SNP_Calculator.py
+++ SNP_Calculator.py
@@ -7,170 +7,135 @@ from Bio.SeqRecord import SeqRecord
 from Bio import SeqIO
 import time
 from Bio import SeqFeature
+from dna_features_viewer import (
+    GraphicFeature,
+    GraphicRecord,
+    CircularGraphicRecord,
+)
+import matplotlib.pyplot as plt
+############################################################################Importation des modules############################################################################
+seqRecord = SeqIO.read('1_S210_consensus_S1A1-3_.fasta', 'fasta')
+seql=len(seqRecord)
+
+
+df=pd.read_csv('SNPs',sep="\t")
+
+long=input('Entrez la taille en nucléotide de la zone : ')
+Longueur=int(long)
 
 
 
-############################################################################################################################################################################################
-###########################################################################################################################################################################################
-############################################################################################################################################################################################
-
-débutduprogramme = time.perf_counter()
-#Permet de calculer le temps d'action du programme
-
-
-#####################Version interactive###############################
-#Seqinteret=input('Entrez le nom de votre séquence:  ')
-#seqRecord3 = SeqIO.read('Seqinterest', 'fasta')
-#seql=len(seqRecord3)
-#tableau=input('Entrez le nom de votre document contenant les SNPs:  ')
-#df=pd.read_csv('tableau',sep="\t")
+for i in df.index:
+    if df.sequence_3_PosInContg[i]==0:
+        df.drop(i, inplace=True)
 
 
 
-#####################Version devellopeur###############################
-seqRecord3 = SeqIO.read('Sequence4s11ououi.fasta', 'fasta')
-seql=len(seqRecord3)
+
+df.sort_values(by=['sequence_3_GenWidePos3'], inplace=True)
 
 
-#print(seql)
-#On importe la séquence d'interet et on calcule sa taille
-df=pd.read_csv('SNPS',sep="\t")
-#On récupère le Document contenant les snps
-number=[1,2,3]
-for i in number:
-    indexNames = df[ df[f'sequence_{i}_PosInContg'] == 0 ].index
-    df.drop(indexNames , inplace=True)
-
-#cette partie permet de supprimer les lignes avec des valeurs à zéro (C'est normalement impossible, mais mieux vaut ne pas prendre de risque)
-
-    
-
-snp_pattern= df[f'SNP_pattern'].tolist()
-Position=[]
-A=[]
-B=[]
-R=[]
-for label, row in df.iterrows():
-    
-    position=df.sequence_3_PosInContg[label]
-    Position.append(position)
-    snp=df.SNP_pattern[label]
-    patternpA=snp[0]
-    A.append(patternpA)
-    patternpB=snp[1]
-    B.append(patternpB)
-    patternR=snp[2]
-    R.append(patternR)
-
-data = {'SNP':snp_pattern,'A':A,'B':B,'R':R,'Position':Position}
-dfr = pd.DataFrame(data)
-dfr['index'] = dfr.index
+liste= df['sequence_3_GenWidePos3'].tolist()
+l=len(liste)
 
 
-for i in range(len(dfr)):
-    if dfr.loc[i,"A"]==dfr.loc[i,"B"]:
-        dfr.drop(i,0,inplace=True)
-        
-dsort=dfr.sort_values(by=['Position'])
-dsort['index'] = dsort.index
+#####on a éliminé les valeures nulles, on s'interessent mtn suelement aux snps
+####on va mtn calculé la densité moyenne des snps
+den=(l/seql)
+densite=(seql/l)
+dens=int(densite)
 
 
-for i in dsort.index:
-    if dsort.A[i]!= dsort.R[i]and dsort.A[i]!= dsort.B[i] and dsort.B[i]!= dsort.R[i]:
-        dsort.drop(i,0,inplace=True)
-simple=[]
-infsnp=[]
-Positions=[]
+Feature=[]
 
 
-for i in dsort.index:
-    Positions.append(dsort.Position[i])
-    if dsort.A[i]==dsort.R[i]:
-        simple.append('A')
-    if dsort.B[i]==dsort.R[i]:
-        simple.append('B')
-
-
-        
-tui=len(Positions)
-
-tur=tui+1
-Gapinf=[]
-Gapsup=[]
-
-for u in range(tui):
-    
-    ty=u+1
-    tu=u-1
-    if ty>=tui:
-        ty=u
-    else:
-        ty=ty
-
-    if tu<0:
-        tu=u
-    else:
-        tu=tu
-    
-    tinf=Positions[u]-Positions[tu]
-    Gapinf.append(tinf)
-    
-    tsup=Positions[ty]-Positions[u]
-    Gapsup.append(tsup)
-    
-    
-   
-data = {'Origine':simple,'Position':Positions,'Gapinf':Gapinf,'Gapsup':Gapsup}
-dfinal = pd.DataFrame(data)
-dfinal.to_csv(f'SNPsfinal.csv')
-dfinal.to_excel(f'SNPsfinal.xlsx')
-
-
-dfy=pd.read_csv('SNPsfinal.csv',sep=",")
-dfy['index'] = dfy.index
-
-yu=len(dfy.index)
-yut=yu-1
-
-listesnp = dfy['Position'].tolist()
-listenature=[]
-listepos=[]
-list_ = list(range(seql))
-oip=list_[0::1000000]
-
-
-yu=len(dfy.index)
-yht=(yu/seql)*100
-#print(f"Densité SNPs sur le génome entier: {yht}%")
-
-listexcl=[]
 lstart=[]
+for i in range(seql):
+    if i%Longueur == 0:
+        lstart.append(i)
 
-for r in oip:
-    pou=0
-    t=r+1000000
-    jr=r/1000000
-    jt=t/1000000
-    
-    lstart.append(f'{jr}-{jt}')
-    
-    for i in listesnp:
-        if r<i<t:
-          pou=pou+1
-    if t == 12000000:
-       tuy=(pou/(seql-11000000))*100
-       listexcl.append(tuy)
+len2=len(lstart)
+len3=len2-1
+lcompteur=[]
+
+for t in range(len(lstart)):
+    g=t+1
+    if g>=len2:
+        g=g-1
     else:
-        tuy=(pou/1000000)*100
-        listexcl.append(tuy)
-
-
+        g=g
+    compteur=0
+    for y in range(len(liste)):
+        if lstart[t]<=liste[y]<=lstart[g]:
+            compteur=compteur+1
+    #if compteur>testden:
+        #Feature.append(GraphicFeature(start=lstart[t], end=lstart[g],color="#ffcccc"))
+    #else:
+        #Feature.append(GraphicFeature(start=lstart[t], end=lstart[g],color="#cffccc"))
+        
+    lcompteur.append(compteur)
     
-    #print(f"Densité SNPs entre la position{r} et {t}: {tuy}")
-listexcl.append(yht)
-lstart.append('densitémoyenne')
-data = {'Fenêtre en MgB':lstart,'Densité en SNP(en %)':listexcl}
-pio = pd.DataFrame(data)
-pio.to_excel(f'SNPsdensity.xlsx')
+positiong=[]
+positiond=[]
+for j in range(len(lcompteur)):
+    b=j*Longueur
+    r=b+Longueur
+    positiong.append(b)
+    positiond.append(r)
+    
+    
+data = {'SNPs':lcompteur,'Start':positiong,'End':positiond}
+dfinal = pd.DataFrame(data)
+dfinal['index'] = dfinal.index
+dfinal.to_excel(f'total_SNPs123.xlsx')
+
+
+
+moyatti=den*Longueur
+moyatt=int(moyatti)
+
+
+
+
+dlarge=dfinal.nlargest(10, ['SNPs'])
+
+newsizestart=[]
+newsizeend=[]
+for i in dlarge.index:
+    h=0
+    f=0
+    h=Longueur+(dlarge.SNPs[i]*Longueur)
+    f=h+Longueur
+    newsizestart.append(h)
+    newsizeend.append(f)    
+
+data = {'Start':newsizestart,'End':newsizeend}
+dnewlarge = pd.DataFrame(data)
+
+
+fichier1 = open(f'Infos8.txt', "a")
+fichier1.write(f'Taille de la zone: {Longueur} pb\n')
+fichier1.write(f'Densité moyenne de snps sur le génome total {den}\n')
+fichier1.write(f'Cela correspont en moyenne à 1 SNP tous les {dens} nucléotides \n')
+fichier1.write(f'Moyenne attendue : {moyatt} SNPs sur chaque zone\n')
+fichier1.write(f'{dnewlarge} \n')
+fichier1.close()
+
+
+#record = GraphicRecord(sequence_length=seql, features=Feature)
+#ax, _ = record.plot(figure_width=25)
+#ax.figure.savefig("SNPs.png")
+print("It's over!")
+
+
+
+        
+
+
+
+        
+
+
+
 
 
